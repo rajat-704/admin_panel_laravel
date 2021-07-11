700788b62f6d8ee8065a17b47332556aeb9633f0
rajat-704 <rajatgumber70@gmail.com>
2021-07-11 19:06:33 +0530

    user published

---
+++ app/Repositories/Admin/UserRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+use App\Models\User;
+use App\Repositories\Admin\BaseRepository;
+
+/**
+ * Class UserRepository
+ * @package App\Repositories\Admin
+*/
+
+class UserRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'email',
+        'password'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return User::class;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('users', \App\Http\Controllers\Admin\UserController::class)->middleware('auth');
